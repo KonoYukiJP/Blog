d018265069d9f9246cbb3bf59340aee2356e1c39
河野優輝 <koyu@u-kiMacBook-Air.local>
2025-07-15 21:50:35 +0900

    1.19

--- send.php
+++ send.php
@@ -1,6 +1,9 @@
-
-
 <?php
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    header('Location: /contact/');
+    exit;
+}
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
